6cf1d3463c4464250acf22cff4a6cc98c4d7fea9
Alex Maese <memaese@hotmail.com>
2019-01-29 15:37:08 -0600

    Enable creation and destruction of session tokens

--- api/session.py
+++ api/session.py
@@ -1,3 +1,31 @@
 #!/usr/bin/python
 
-sessions = {"alex": 0, "denton": 1, "eli": 2, "michael": 3}
+from utils import *
+
+_sessions = {}
+# _sessions = {"alex": 0, "denton": 1, "eli": 2, "michael": 3}
+
+def student_from_session(session_token):
+	if session_token in _sessions:
+		return _sessions[session_token]
+	return None
+
+# if token is not None, use that for testing purposes
+def create_session(student_id, token=None):
+	"""
+	Create a student and put it into the sessions dictionary
+	Returns the session token
+	"""
+	student = get_student_by_id(student_id)
+	if student is None:
+		return False
+
+	if token is None:
+		# TODO
+		token = "random session token"
+	_sessions[token] = student
+	return token
+
+def destroy_session(session_token):
+	if session_token in _sessions:
+		del _sessions[session_token]
