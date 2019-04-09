9c1eb103b5ef4839c056ca61fcd63e8dca9da2c1
Alex Maese <memaese@hotmail.com>
2019-04-09 16:44:01 -0500

    Added a housing component and added it to the router

--- site/RoomDraw/src/app/app-routing.module.ts
+++ site/RoomDraw/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { GroupComponent } from './group/group.component';
+import { HousingComponent } from './housing/housing.component';
 
 const routes: Routes = [
+	{ path: 'housing', component: HousingComponent },
 	{ path: 'group', component: GroupComponent },
 	{ path: 'home', component: HomeComponent },
 	{ path: '', redirectTo: '/home', pathMatch: 'full' }
--- site/RoomDraw/src/app/app.module.ts
+++ site/RoomDraw/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { GroupComponent } from './group/group.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HousingComponent } from './housing/housing.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		HomeComponent,
 		GroupComponent,
-		NavBarComponent
+		NavBarComponent,
+		HousingComponent
 	],
 	imports: [
 		BrowserModule,
