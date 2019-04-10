cb558633547dd7926907744b71e60be94fd37a16
Alex Maese <memaese@hotmail.com>
2019-04-10 08:39:20 -0500

    Dynamically add floor plans when a dorm is chosen

--- site/RoomDraw/src/app/housing/housing.component.ts
+++ site/RoomDraw/src/app/housing/housing.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-housing',
-  templateUrl: './housing.component.html',
-  styleUrls: ['./housing.component.css']
+	selector: 'app-housing',
+	templateUrl: './housing.component.html',
+	styleUrls: ['./housing.component.css']
 })
 export class HousingComponent implements OnInit {
 
-  constructor() { }
+	halls = [
+		{id: 0, name: "Lemke Hall", code: "LEM"},
+		{id: 1, name: "Newman Hall", code: "NEW"},
+		{id: 2, name: "St. Joseph Hall", code: "STJ"},
+		{id: 3, name: "St. Michael Hall", code: "STM"},
+		{id: 4, name: "Wolf Hall", code: "WOLF"}
+	];
+	current_plan = null;
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.loadFloorPlan(0);
+	}
 
+	loadFloorPlan(id) {
+		this.current_plan = `/assets/floorplans/${this.halls[id].code}.jpg`;
+	}
 }
