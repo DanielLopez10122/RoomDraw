8dd62b0a78bdd1903b2b7f08e25a0813593980f3
Alex Maese <memaese@hotmail.com>
2019-12-05 19:55:38 -0600

    Allow OPTIONS through AuthenticationMiddleware

--- api/middleware/authentication.py
+++ api/middleware/authentication.py
@@ -9,7 +9,9 @@ def authenticated(session_token):
 
 class AuthenticationMiddleware(object):
 	def process_request(self, request, response):
+		if request.method == 'OPTIONS':
+			return
+
 		session_token = get_session(request)
 		if not authenticated(session_token):
 			raise HTTPUnauthorized(title="Not authenticated", challenges=["Bearer"])
-
