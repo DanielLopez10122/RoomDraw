1d3f437fdd508b7e3dc564d984331d8f5ea448aa
Alex Maese <memaese@hotmail.com>
2019-12-20 22:10:45 -0600

    Revert "Reuse open sessions" due to cache issues
    This reverts commit 85bfd8a4828559ff1f361213a5ab907578fd030c.

--- api/private/sql.py
+++ api/private/sql.py
@@ -4,8 +4,6 @@ import sqlalchemy
 import sqlalchemy.orm
 import private.constants
 
-current_session = None
-
 def get_dialect():
 	return private.constants.sql_dialect
 def get_driver():
@@ -22,13 +20,8 @@ def get_db():
 	return private.constants.sql_db
 
 def sql_create_session():
-	global current_session
-	if current_session is not None:
-		return current_session
-
 	url = sqlalchemy.engine.url.URL(get_driver(), get_user(),
 			get_password(), get_host(), get_port(), get_db())
 	engine = sqlalchemy.create_engine(url)
 	Session = sqlalchemy.orm.sessionmaker(bind=engine)
-	current_session = Session()
-	return current_session
+	return Session()
